810d6d0bdf0d24c3a5f35022dd62e8e5f7e7a8f0
adrian92612 <adrian.villamin0612@gmail.com>
2024-01-08 21:16:49 +0800

    initial app templates and files

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: path.resolve(__dirname, "src/index.js"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "src/template.html",
+    }),
+  ],
+};
